6e5ede47babd995aca69184d20ed9cf1efddd985
kallol1988 <kallol.banerjee@tier5.in>
2023-08-04 10:37:20 +0530

    new profile pages v1.01

--- src/pages/Profile/ProfileSidebar.js
+++ src/pages/Profile/ProfileSidebar.js
@@ -1,10 +1,82 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import profile from "../../assets/images/profile.png";
+import profileIcon from "../../assets/images/camelIcon.png";
+import reward from "../../assets/images/rewardStar.png";
+import edits from "../../assets/images/edits.png";
+import qr from "../../assets/images/qr-code-2.png";
+import routIcon1 from "../../assets/images/crypto.png";
+import routIcon2 from "../../assets/images/loveIcon.png";
+import routIcon3 from "../../assets/images/credit-card-4.png";
+import routIcon4 from "../../assets/images/location@2x.png";
 
 const ProfileSidebar = () => {
     return (
-        <>
-            <h2>Profile Sidebar</h2>
-        </>
+        <div className='profileSidebarWraper'>
+            <div className='sideProfileTop'>
+                <div className='profileImgWraper'>
+                    <span className='profileImg'>
+                        <img src={profile} className='profileImages' alt='' />
+                        <img src={profileIcon} className='profileCamel' alt=''/>
+                    </span>
+                    <p className='profileName'>Aisha Fahd</p>
+                </div>
+                <div className='profilePointWraper'>
+                    <span className='editPointsSec'>
+                        <img src={edits} alt='' />
+                    </span>
+                    <div className='pointsProfileWrapers'>
+                        <h3>300</h3>
+                        <img src={reward} alt='' />
+                    </div>
+                    <p className='converstionRate'>Equals to <span>3.00 SAR</span></p>
+                    <p className='numberInfo'>Your Sawani Rewards Number</p>
+                    <div className='codeQr'>
+                        <img src={qr} className='codeScan' />E01N-98OL-3D5U
+                    </div>
+                </div>
+            </div>
+
+            <div className='sideProfileMid'>
+                <ul className='profileRouts'>
+                    <li className="activated">
+                        <span className='iconName'>
+                            <img src={routIcon1} className='routIcon' />
+                        </span>
+                        <span className='nameLink'>Points Analysis</span>
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </li>
+                    <li className="">
+                        <span className='iconName'>
+                            <img src={routIcon2} className='routIcon' />
+                        </span>
+                        <span className='nameLink'>Favorites</span>
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </li>
+                    <li className="">
+                        <span className='iconName'>
+                            <img src={routIcon3} className='routIcon' />
+                        </span>
+                        <span className='nameLink'>My Cards</span>
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </li>
+                    <li className="">
+                        <span className='iconName'>
+                            <img src={routIcon4} className='routIcon' />
+                        </span>
+                        <span className='nameLink'>My Addresses</span>
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </li>
+                </ul>
+            </div>
+
+            <div className='refferWraper'>
+                <h4>Want more benefits!</h4>
+                <p>Refer a friend and enjoy more rewards <br /> together</p>
+                <button className='refferBtn'>Refer a friend</button>
+            </div>
+
+        </div>
     );
 };
 
--- src/pages/Profile/RewardsPoint.js
+++ src/pages/Profile/RewardsPoint.js
@@ -2,13 +2,79 @@ import React from 'react';
 import Header from '../../components/common/layout/Header/Header';
 import Footer from '../../components/common/layout/Footer';
 import ProfileSidebar from './ProfileSidebar';
+import circular from "../../assets/images/circular.png";
+import smallLogo from "../../assets/images/smLogo.png";
+import icon1 from "../../assets/images/icons1.png";
+import icon2 from "../../assets/images/icons2.png";
+import icon3 from "../../assets/images/icons3.png";
+import question from "../../assets/images/questionBack.png";
 
 const RewardsPointPage = () => {
     return (
         <>
             <Header />
-            <ProfileSidebar />
-            <h2>Rewards Point Page</h2>
+            <div className="dashboardMidContent profilePages">
+             <ProfileSidebar />
+                <div className='profileRightWraper'>
+                    <div className='pointAnalysisWraper'>
+                        <div className='pointTabWraper'>
+                            <div className='tabWrapers'>
+                                <ul className='pointsTabs'>
+                                    <li className='activeTab'>Month</li>
+                                    <li className=''>6 Months</li>
+                                    <li className=''>Annual</li>
+                                </ul>
+                                <p className='earnedInfos'><span>See how much you’ve earned</span></p>
+                                <div className='pointDislayWraper'>
+                                    <div className='circlePoints'>   
+                                        <img src={circular} className='circleBack' alt='' />
+                                        <span className='pointsMainInfo'>
+                                            <span className='pointsSmLogo'>
+                                                <img src={smallLogo} className='smLogo' alt='' />
+                                            </span>
+                                            <p className='pointsValue'>2023</p>
+                                            <p className='pointsNames'>Point Balance</p>
+                                            <p className='equalConversion'>Equals to <span>20.32</span> SAR</p>
+                                        </span>
+                                    </div>
+                                    <div className='pointsListings'>
+                                        <ul className='pointListUl'>
+                                            <li>
+                                                <span className='pointListIcons'>
+                                                    <img src={icon1} className='iconsLists' alt='' />
+                                                </span>
+                                                <span className='pointsName'>Redeemed</span>
+                                                <span className='pointsPercent'>15%</span>
+                                            </li>
+                                            <li className='activated'>
+                                                <span className='pointListIcons'>
+                                                    <img src={icon2} className='iconsLists' alt='' />
+                                                </span>
+                                                <span className='pointsName'>Purchases</span>
+                                                <span className='pointsPercent'>25%</span>
+                                            </li>
+                                            <li>
+                                                <span className='pointListIcons'>
+                                                    <img src={icon3} className='iconsLists' alt='' />
+                                                </span>
+                                                <span className='pointsName'>Weekly logins & other</span>
+                                                <span className='pointsPercent'>35%</span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <button className='pointDetail'>Details</button>
+                            </div>
+                        </div>
+
+                        <div className='questionsWrapers'>
+                            <h3>We want to get to know you</h3>
+                            <button className='qusBtn'>Answer Questionnaire</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </>
     );
